7b0260f601b9e48aeaffe77f87d357d4b8408b5e
Hagop Ket. <116393832+Hagket@users.noreply.github.com>
2024-12-02 18:20:46 -0500

    Update Problem_12.py

--- Problems/Problem_12.py
+++ Problems/Problem_12.py
@@ -1,24 +1,23 @@
 # Problem 12#
+#This solution is too slow. The divisors function needs to be optimized to find divisors quicker#
 import sys
 
-
-def divisor(num):
+def divisors(num):
     divisors = []
-
+    print(num)
     for i in range(1, num + 1):
-        if (num % i == 0):
-            divisors.append(i)
+        if (num % i == 0): divisors.append(i)
     if (len(divisors) >= 500):
         print("Triangular number with over 500 divisors: " + str(num))
         print(divisors)
         sys.exit()
 
 
-num = 0
+triNum = 0
 i = 1
 while (True):
-    divisor(num + i)
-    num += i
+    divisors(triNum)
+    triNum += i
     i += 1
 
 print("Program is finished.")
