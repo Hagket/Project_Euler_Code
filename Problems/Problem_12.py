4e8caa359646f3d08bdaeb28f766142a16bc3222
Hagop Ket. <116393832+Hagket@users.noreply.github.com>
2024-12-02 19:00:53 -0500

    Update Problem_12.py
    Added prime_factors() to further optimize the script

--- Problems/Problem_12.py
+++ Problems/Problem_12.py
@@ -1,16 +1,44 @@
 # Problem 12#
 #This solution is too slow. The divisors function needs to be optimized to find divisors quicker#
 import sys
+import math
 
-def divisors(num):
-    divisors = []
-    print(num)
-    for i in range(1, num + 1):
-        if (num % i == 0): divisors.append(i)
-    if (len(divisors) >= 500):
-        print("Triangular number with over 500 divisors: " + str(num))
-        print(divisors)
-        sys.exit()
+def prime_factors(n):
+  primeFactors = []
+  divisors = []
+  
+  duplicateCount = 0
+  while n % 2 == 0:
+    duplicateCount += 1
+    if duplicateCount == 1: 
+      divisors.append(2)
+    primeFactors.append(2)
+    n = n // 2
+  for i in range(3, int(math.sqrt(n)) + 1, 2):
+    duplicateCount = 0
+    while n % i == 0:
+      duplicateCount += 1
+      if duplicateCount == 1: 
+        divisors.append(i)
+      primeFactors.append(i)
+      n = n // i
+  if n > 2:
+      primeFactors.append(n)
+      divisors.append(n)
+    
+  return primeFactors
+        
+
+def divisors(num): #main function. prime_factors has not been connected yet.
+  divisors = []
+  print(num)
+  for i in range(1, num + 1):
+      if (num % i == 0): 
+        divisors.append(i)
+  if (len(divisors) >= 500):
+      print("Triangular number with over 500 divisors: " + str(num))
+      print(divisors)
+      sys.exit()
 
 
 triNum = 0
@@ -19,5 +47,3 @@ while (True):
     divisors(triNum)
     triNum += i
     i += 1
-
-print("Program is finished.")
